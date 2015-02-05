06b03083c71fa43a14d186e434c6f2625133a983
Louis Zhu <zhuxiaofan@gmail.com>
2015-02-05 20:34:19 +0800

    add Dating.swift and update README

---
+++ Dating.swift
@@ -0,0 +1,108 @@
+//
+//  Dating.swift
+//  
+//
+//  Created by Louis Zhu on 15/2/5.
+//
+//
+
+import Foundation
+
+
+public extension Int {
+    
+    private func newEmptyDateComponents() -> NSDateComponents! {
+        var dateComponents = NSDateComponents()
+        dateComponents.year = 0
+        dateComponents.month = 0
+        dateComponents.day = 0
+        dateComponents.hour = 0
+        dateComponents.minute = 0
+        dateComponents.second = 0
+        return dateComponents
+    }
+    
+    var year: NSDateComponents {
+        let components = self.newEmptyDateComponents()
+        components.year = self
+        return components
+    }
+    
+    var month: NSDateComponents {
+        let components = self.newEmptyDateComponents()
+        components.month = self
+        return components
+    }
+    
+    var day: NSDateComponents {
+        let components = self.newEmptyDateComponents()
+        components.day = self
+        return components
+    }
+    
+    var hour: NSDateComponents {
+        let components = self.newEmptyDateComponents()
+        components.hour = self
+        return components
+    }
+    
+    var minute: NSDateComponents {
+        let components = self.newEmptyDateComponents()
+        components.minute = self
+        return components
+    }
+    
+    var second: NSDateComponents {
+        let components = self.newEmptyDateComponents()
+        components.second = self
+        return components
+    }
+}
+
+
+public extension NSDate {
+    
+    private convenience init(date otherDate: NSDate) {
+        let timeInterval = otherDate.timeIntervalSince1970
+        self.init(timeIntervalSince1970: timeInterval)
+    }
+    
+    convenience init(year: Int, month: Int, day: Int, hour: Int = 0, minute: Int = 0, second: Int = 0) {
+        let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
+        let components = NSDateComponents()
+        components.year = year
+        components.month = month
+        components.day = day
+        components.hour = hour
+        components.minute = minute
+        components.second = second
+        let date = calendar!.dateFromComponents(components)
+        self.init(date: date!)
+    }
+    
+}
+
+func + (left: NSDate, right: NSDateComponents) -> NSDate {
+    let calendar = NSCalendar.currentCalendar()
+    var components = calendar.components(.YearCalendarUnit | .MonthCalendarUnit | .DayCalendarUnit | .HourCalendarUnit | .MinuteCalendarUnit | .SecondCalendarUnit, fromDate: left)
+    components.year += right.year
+    components.month += right.month
+    components.day += right.day
+    components.hour += right.hour
+    components.minute += right.minute
+    components.second += right.second
+    return calendar.dateFromComponents(components)!
+}
+
+func - (left: NSDate, right: NSDateComponents) -> NSDate {
+    let calendar = NSCalendar.currentCalendar()
+    var components = calendar.components(.YearCalendarUnit | .MonthCalendarUnit | .DayCalendarUnit | .HourCalendarUnit | .MinuteCalendarUnit | .SecondCalendarUnit, fromDate: left)
+    components.year -= right.year
+    components.month -= right.month
+    components.day -= right.day
+    components.hour -= right.hour
+    components.minute -= right.minute
+    components.second -= right.second
+    return calendar.dateFromComponents(components)!
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Dating
+
 A Swift tool to simplify date and time calculation. OS X and iOS supported.
+
+When you deal with date and time with Dating, just consider the real world years, months, days, hours, minutes and seconds. And the code will be more natural than the ordinary `NSDate` way.
+
+Dating creates no new class and type, just extensions of `NSDate`, `NSDateComponents` and `Int`. So using Dating is simple, no need to consider modifying your existing data types or structures.
+
+
+## Installation
+
+Directly add the `Dating.swift` file to your project.
+
+## How to use
+
+Create a date with years, months, days, hours(optional), minutes(optional), and seconds(optional).
+
+```swift
+let dateA = NSDate(year: 2014, month: 10, day: 27)
+let dateB = NSDate(year: 2014, month: 10, day: 27, hour: 3, minute: 13)
+```
+
+Calculate date use `+` and `-` operations.
+
+```swift
+let dateC = dateA + 1.year
+let dateD = dateB - 25.second
+```
+
+The operations can be chained, of course.
+
+```swift
+let dateE = dateA + 2.year - 3.month + 5.day - 12.hour - 1.minute + 20.second
+```
+
+## License
+
+This code is distributed under the terms and conditions of the [MIT license](LICENSE). 
+
